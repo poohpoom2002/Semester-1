1fce5ab132464538c882428edb3edf3cb6fb0a10
Phuriphut Phokasawat <phuriphut2002@yahoo.com>
2021-08-12 22:39:36 +0700

    Week one commit

--- PrimeNumber-1.c
+++ PrimeNumber-1.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 int main(){
     int n,count=0;
-    scanf("%d",&n);
-    for(int i=2;i<=n;i++){
-        if(n%i==0){
-            count++;
+    do{
+        scanf("%d",&n);
+        if(n==-99){
+            break;
+        }
+        for(int i=2;i<=n;i++){
+           if(n%i==0){
+               count++;
+            }
+        }
+       if(count==1){
+           printf("This number is a prime number.\n");
+        }
+       else{
+           printf("This number is not a prime number.\n");
         }
     }
-    if(count==1){
-        printf("This number is a prime number.");
-    }
-    else{
-        printf("This number is not a prime number.");
-    }
-    
+    while(n!=-99);
+
     return 0;
 }
--- PrimeNumber-2.c
+++ PrimeNumber-2.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main(){
     int n,i;
-    scanf("%d",&n);
-    for(i=2;i<=n;i++){
-        if(n%i==0 && i!=n){
-            printf("This number is not a prime number.");
+    do{
+        scanf("%d",&n);
+        if(n==-99){
             break;
         }
+        for(i=2;i<=n;i++){
+            if(n%i==0 && i!=n){
+                printf("This number is not a prime number.\n");
+                break;
+            }
+        }
+        if(i>n){
+            printf("This number is a prime number.\n");
+        }
     }
-    if(i>n){
-        printf("This number is a prime number.");
-    }
+    while(n!=-99);
     
     return 0;
 }
